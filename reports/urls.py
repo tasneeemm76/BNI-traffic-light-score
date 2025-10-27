45ad12e924ef4750273a72c546c7af5c764a49d8
tasneeemm76 <tasneembawaji03@gmail.com>
2025-10-27 14:41:46 +0530

    url change

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload-excel/', views.upload_excel, name='upload_excel'),
+    path('', views.upload_excel, name='upload_excel'),
     path('dashboard/', views.upload_dashboard, name='upload_dashboard'),
 ]
