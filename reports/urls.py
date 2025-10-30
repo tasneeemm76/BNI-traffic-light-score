356ca0bd184b800098636c1fe9780c3b530d3675
Tasneem Bawaji <156578790+tasneeemm76@users.noreply.github.com>
2025-10-30 17:06:48 +0530

    Update urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('reports/months/', views.months_index, name='reports_months'),
     path('reports/<int:year>/<int:month>/', views.list_reports_month, name='list_reports_month'),
     path('delete_all/', views.delete_all_reports, name='delete_all_reports'),
-   # path('reports/summary/', views.reports_summary , name='reports_summary'),
+    path('reports/summary/', views.reports_summary , name='reports_summary'),
 ]
