c4c113949bf089a00289a0bad08813fe639b2462
Tasneem Bawaji <156578790+tasneeemm76@users.noreply.github.com>
2025-10-31 16:44:22 +0530

    report

--- reports/urls.py
+++ reports/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from . import views
+from .views import upload_file
+
 
 urlpatterns = [
-    path('', views.upload_excel, name='upload_excel'),
-    path('reports/', views.list_reports, name='list_reports'),
-    path('reports/months/', views.months_index, name='reports_months'),
-    path('reports/<int:year>/<int:month>/', views.list_reports_month, name='list_reports_month'),
-    path('delete_all/', views.delete_all_reports, name='delete_all_reports'),
-    path('reports/summary/', views.reports_summary , name='reports_summary'),
+	path('', upload_file, name='upload_file'),
 ]
+
+
